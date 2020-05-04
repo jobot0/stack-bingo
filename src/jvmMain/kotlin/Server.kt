2ddaaef5ac56e55150fbb270e65dc474afe1b33b
jobot0 <me@jobo.to>
2020-05-04 19:19:26 +0200

    :tada: Basic src folder

---
+++ src/jvmMain/kotlin/Server.kt
@@ -0,0 +1,20 @@
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main() {
+    embeddedServer(Netty, 9090) {
+        routing {
+            get("/") {
+                call.respondText(
+                    this::class.java.classLoader.getResource("index.html")!!.readText(),
+                    ContentType.Text.Html
+                )
+            }
+        }
+    }.start(wait = true)
+}
