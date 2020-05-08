2afe89845bc62adc3dd4d2ea5dcd599343eb979b
jobot0 <me@jobo.to>
2020-05-08 18:04:34 +0200

    :sparkles: Add example code to work between server and front :)

--- server/app/app.go
+++ server/app/app.go
@@ -26,7 +26,7 @@ func main() {
 	router := gin.Default()
 
 	// Serve frontend static files
-	router.Use(static.Serve("/", static.LocalFile("./front/build", true)))
+	router.Use(static.Serve("/", static.LocalFile("../../front/build", true)))
 
 	// Setup route group for the API
 	api := router.Group("/api")
