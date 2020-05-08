e819db8683013d3f49151dc803791d8fe9fa7ed1
jobot0 <me@jobo.to>
2020-05-08 11:58:19 +0200

    :sparkles: Add dumb route to prepare front/server interaction

--- server/app/app.go
+++ server/app/app.go
@@ -4,23 +4,24 @@ import (
 	"net/http"
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
-	//"github.com/go-pg/pg/v9"
-	//"stack-bingo/core"
-	//_dbDumbRepository "stack-bingo/data"
+	"github.com/go-pg/pg/v9"
+	"stack-bingo/core"
+	_dbDumbRepository "stack-bingo/data"
 )
 
+type jsonDumb struct {
+	Name string `json:"name" binding:"required"`
+}
 
 func main() {
-	//db := pg.Connect(&pg.Options{
-	//	Database: "stack-bingo",
-	//	User: "postgres",
-	//	Password: "Pass2020!",
-	//})
+	db := pg.Connect(&pg.Options{
+		Database: "stack-bingo",
+		User: "postgres",
+		Password: "Pass2020!",
+	})
 
-	//dumbRepo := _dbDumbRepository.NewDbDumbRepository(db)
-	//dumbu := core.NewCreateDumbUsecase(dumbRepo)
-	//d := core.Dumb{Name: "oulala"}
-	//dumbu.Execute(&d)
+	dumbRepo := _dbDumbRepository.NewDbDumbRepository(db)
+	dumbu := core.NewCreateDumbUsecase(dumbRepo)
 	// Set the router as the default one shipped with Gin
 	router := gin.Default()
 
@@ -30,6 +31,12 @@ func main() {
 	// Setup route group for the API
 	api := router.Group("/api")
 	{
+		api.POST("/dumb", func(c *gin.Context) {
+			var toto jsonDumb
+			c.BindJSON(&toto)
+			d := core.Dumb{Name: toto.Name}
+			dumbu.Execute(&d)
+		})
 		api.GET("/", func(c *gin.Context){
 			c.JSON(http.StatusOK, gin.H{
 				"message": "pong",
